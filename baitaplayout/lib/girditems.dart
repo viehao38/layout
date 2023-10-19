93c5ab0e778fe65793819da2bb1e47dc89d1cf4b
WuanNguyen <nguyenhuanhtk2003@gmail.com>
2023-10-19 19:58:14 +0700

    edit girditeam and iteam

---
+++ baitaplayout/lib/girditems.dart
@@ -0,0 +1,27 @@
+import 'package:baitaplayout/item.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Girditeams extends StatelessWidget {
+  const Girditeams({super.key});
+   Color _getRandomColor() {
+    final Random random = Random();
+    return Color.fromARGB(
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
+    );// random mau
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 4,
+      children: List.generate(8, (index){
+        final randomColor = _getRandomColor();// random mau
+        return MyItem(randoms: randomColor);
+      }),
+      );
+  }
+}
--- baitaplayout/lib/main.dart
+++ baitaplayout/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:baitaplayout/girditems.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+      ), 
+      home: Girditeams(),
     );
   }
 }
