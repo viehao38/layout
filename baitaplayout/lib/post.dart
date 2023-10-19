e0f16f4e527ef4de68040e28d0ea8b3dbf7116a8
QuangDuy47 <nguyenduy4157@gmail.com>
2023-10-19 16:58:49 +0700

    class

---
+++ baitaplayout/lib/post.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThuocTinh{
+  final String name;
+  final String maso;
+  final int age;
+  final Image hinhanh;
+  ThuocTinh({required this.name,required this.age,required this.maso,required this.hinhanh});
+}
